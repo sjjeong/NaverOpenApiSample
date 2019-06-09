bb51312a91f4ecd085341827df204308b9246abc
sjjeong <sjjeong1225@gmail.com>
2019-06-09 17:34:21 +0900

    add Application class

---
+++ app/src/main/java/com/googry/naveropenapisample/MainApplication.kt
@@ -0,0 +1,23 @@
+package com.googry.naveropenapisample
+
+import android.app.Application
+import com.dino.library.di.getDinoNetworkModule
+import com.dino.library.ext.setupKoin
+import com.googry.naver.domain.di.dinoNaverDomainModule
+import com.googry.naver.remote.di.dinoNaverRemoteModule
+import com.googry.naver.repository.di.dinoNaverRepositoryModule
+
+class MainApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        setupKoin(
+            this,
+            getDinoNetworkModule("https://openapi.naver.com/"),
+            dinoNaverDomainModule,
+            dinoNaverRepositoryModule,
+            dinoNaverRemoteModule
+        )
+    }
+
+}
